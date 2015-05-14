4068851eb56a230a8ce5685e24fb88a4d2903e3f
astroeng <dschacht@gmail.com>
2015-05-14 18:15:17 -0400

    Moved the pin assignments to the config.h file. Started to implement the software serial interface. Updated a few things that were discovered during testing after soldering on the rotary knob. Mainly limiting the PWM range so it does not wrap and modding the active channel so that it does wrap.

--- LED_PWM_Controller.ino
+++ LED_PWM_Controller.ino
@@ -1,20 +1,13 @@
 #include <primitive_scheduler.h>
 #include <rotary_knob_decoder.h>
+#include <SoftwareSerial.h>
+#include "config.h"
 
 #define ACTIVE     45
 #define NOT_ACTIVE 44
 
-#define USER_LED 13
-
-#define CHANNELS 4
-#define CHANNEL_1_LED A5
-#define CHANNEL_1_PWM A5
-#define CHANNEL_2_LED A5
-#define CHANNEL_2_PWM A5
-#define CHANNEL_3_LED A5
-#define CHANNEL_3_PWM A5
-#define CHANNEL_4_LED 4
-#define CHANNEL_4_PWM 5
+#define MAX_PWM 255
+#define MIN_PWM 0
 
 typedef struct {
   unsigned char pwm;
@@ -26,19 +19,24 @@ channel_type channel_pins[CHANNELS] = {CHANNEL_1_PWM, CHANNEL_1_LED,
                                        CHANNEL_3_PWM, CHANNEL_3_LED,
                                        CHANNEL_4_PWM, CHANNEL_4_LED};
 
-channel_type channel_state[CHANNELS] = {255, 0,
-                                        255, 0,
-                                        255, 0,
-                                        255, 0};
-
-#define ROT_KNOB_RED    A7
-#define ROT_KNOB_GREEN  A6
-#define ROT_KNOB_BUTTON 12
-#define ROT_KNOB_A      2
-#define ROT_KNOB_B      3
+channel_type channel_state[CHANNELS] = {MAX_PWM/4, 0,
+                                        MAX_PWM/4, 0,
+                                        MAX_PWM/4, 0,
+                                        MAX_PWM/4, 0};
 
 PrimitiveScheduler schedule;
-RotaryKnobDecoder  knob;
+
+RotaryKnobDecoder  knob(ROT_KNOB_A, 
+                        ROT_KNOB_B, 
+                        ROT_KNOB_BUTTON, 
+                        ROT_KNOB_RED, 
+                        ROT_KNOB_GREEN);
+
+SoftwareSerial mySerial(SOFTWARE_SERIAL_RX, 
+                        SOFTWARE_SERIAL_TX);
+
+char serialPosition = 0;
+char serialBuffer[64];
 
 char rot_knob_button_state;
 char active_channel;
@@ -48,6 +46,7 @@ char user_led_state;
 void setup()
 {
   Serial.begin(9600);
+  mySerial.begin(9600);
   
   int i;
   for (int i = 0; i < CHANNELS; i++)
@@ -61,21 +60,14 @@ void setup()
   schedule.addTask(blink_channel_led, 250);
   schedule.addTask(update_led_pwm, 125);
   schedule.addTask(button_check, 100);
-  schedule.addTask(stats_task, 2000);
-
-  knob = RotaryKnobDecoder(ROT_KNOB_A, 
-                           ROT_KNOB_B, 
-                           ROT_KNOB_BUTTON, 
-                           ROT_KNOB_RED, 
-                           ROT_KNOB_GREEN);
+  //schedule.addTask(stats_task, 2000);
   
   rot_knob_button_state = NOT_ACTIVE;
   user_led_state = 0;
-  active_channel = 3;
+  active_channel = 2;
 
   attachInterrupt(0, rot_knob, CHANGE);
   attachInterrupt(1, rot_knob, CHANGE);
-
 }
 
 /* Run the Ardiono loop function.
@@ -83,6 +75,14 @@ void setup()
 void loop()
 {
   schedule.run();
+  
+  if (mySerial.available())
+  {
+    serialBuffer[serialPosition] = mySerial.read();
+    serialPosition++;
+    serialBuffer[serialPosition] = 0;
+    Serial.println(serialBuffer);
+  }
 }
 
 /* The channel that is tied to the rotary knob should blink.
@@ -117,7 +117,7 @@ void button_check()
   if ((button_state == LOW) && (rot_knob_button_state == NOT_ACTIVE))
   {
     channel_state[active_channel].led = 0;
-    active_channel++;
+    active_channel = (active_channel + 1) % CHANNELS;
     rot_knob_button_state = ACTIVE;
   }
   else if (button_state == HIGH)
@@ -133,11 +133,11 @@ void rot_knob()
 {
   int knob_direction = knob.read();
   
-  if (knob_direction == CLOCKWISE)
+  if (knob_direction == CLOCKWISE && channel_state[active_channel].pwm < MAX_PWM)
   {
     channel_state[active_channel].pwm++;
   }
-  else if (knob_direction == ANTICLOCKWISE)
+  else if (knob_direction == ANTICLOCKWISE && channel_state[active_channel].pwm > MIN_PWM)
   {
     channel_state[active_channel].pwm--;
   }
@@ -146,8 +146,11 @@ void rot_knob()
 
 void stats_task()
 {
+  //TCCR0B = (TCCR0B & 0xF8) | 0x05;
+  //Serial.print((int)TCCR0B & 0x07);
   Serial.print((int)active_channel);
   Serial.print(" : ");
   Serial.println((int)channel_state[active_channel].pwm);
-
+  
+  mySerial.println("Hello");
 }
---
+++ config.h
@@ -0,0 +1,27 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define USER_LED 13
+
+#define CHANNELS 4
+
+#define CHANNEL_1_LED 11
+#define CHANNEL_1_PWM 10
+#define CHANNEL_2_LED 8
+#define CHANNEL_2_PWM 9
+#define CHANNEL_3_LED 7
+#define CHANNEL_3_PWM 6
+#define CHANNEL_4_LED 4
+#define CHANNEL_4_PWM 5
+
+#define ROT_KNOB_RED    A7
+#define ROT_KNOB_GREEN  A6
+#define ROT_KNOB_BUTTON 12
+#define ROT_KNOB_A      3
+#define ROT_KNOB_B      2
+
+#define SOFTWARE_SERIAL_RX A0
+#define SOFTWARE_SERIAL_TX 13
+
+#endif
